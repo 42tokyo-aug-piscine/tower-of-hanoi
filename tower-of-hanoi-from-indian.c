dc88562d2743bca1c84aaa61dbd460b0e35c313a
yuki sakuma <kumasan.04.24.14@gmail.com>
2020-07-18 23:58:27 +0900

    ハノイの塔別解
    問題を解いてはいませんが、参考になればと思い、上げておきます。
    youtubeでインド人が解説してるのを見つけたものです！

---
+++ tower-of-hanoi-from-indian.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void towers(int, char, char, char);
+
+int main(void){
+    int num;
+    scanf("%d", &num);
+    towers(num, 'A', 'C', 'B');
+}
+
+void towers(int num, char frompeg, char topeg, char auxpeg){
+    if(num == 1){
+        printf("ディスク1を %c から %cへ 動かす\n", frompeg, topeg);
+        return;
+    }
+    towers(num - 1, frompeg, auxpeg, topeg);
+    printf("ディスク%dを %c から %cへ動かす\n", num, frompeg, topeg);
+    towers(num - 1, auxpeg, topeg, frompeg);
+}
