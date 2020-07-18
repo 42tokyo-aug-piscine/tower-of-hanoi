4a1607d71507c3882575f1e6d6f13aa513cd4dcd
SIORANGE <68314418+SIORANGE@users.noreply.github.com>
2020-07-18 16:25:05 +0900

    いろいろ間違えました。これが送りたかった

--- hanoi.c
+++ hanoi.c
@@ -1,18 +1,16 @@
 
 #include <stdio.h>
+#include <string.h>
+
 
 void hanoi(int , int , int* , int* , int*);
 
 int fin = 0,count = 0;
 
-//動くには動きますが不要部分めちゃくちゃ多いです・・・多分半分くらいは短くできます笑
-//とりあえずgithub使ってみたくてコード編集する前にあげちゃいましたので、時間あったら改訂版出します〜
-//gitの使い方がむず過ぎて多分無理やけど・・・
-
 int main(void)
 {
     int n , t , i=0 , tower[3][17] ,a=0,b=0,c=0;
-    char ans[3][40];
+    char ans[3][40] = { "" , "" , ""} , p[40];
 
     scanf("%d %d",&n,&t);
 
@@ -23,27 +21,44 @@ int main(void)
         tower[2][i]=0;
         i++;
       }
-      
+
+
     hanoi(n,t,tower[0],tower[1],tower[2]);
 
     i=n-1;
     while(i>=0){
       if(tower[0][i]==1){
-        if(a==0) sprintf(ans[0], "%d", i);
-        else sprintf(ans[0], "%s %d",ans[0] , i);
+        if(a==0) sprintf(ans[0], "%d", i+1);
+        else {
+            strcpy(p,ans[0]);
+            sprintf(ans[0], "%s %d",p , i+1);
+        }
         a++;
       }
+
       if(tower[1][i]==1){
-        if(b==0) sprintf(ans[1], "%d", i);
-        else sprintf(ans[1], "%s %d",ans[1] , i);
+        if(b==0) sprintf(ans[1], "%d", i+1);
+        else {
+            strcpy(p,ans[1]);
+            sprintf(ans[1], "%s %d",p , i+1);
+        }
         b++;
       }
       if(tower[2][i]==1){
-        if(c==0) sprintf(ans[2], "%d", i);
-        else sprintf(ans[2], "%s %d",ans[2] , i);
+        if(c==0) sprintf(ans[2], "%d", i+1);
+        else {
+            strcpy(p,ans[2]);
+            sprintf(ans[2], "%s %d",p , i+1);
+        }
         c++;
       }
+        i--;
     }
+
+
+    if(strcmp(ans[0],"")==0) strcpy(ans[0],"-");
+    if(strcmp(ans[1],"")==0) strcpy(ans[1],"-");
+    if(strcmp(ans[2],"")==0) strcpy(ans[2],"-");
 
     printf("%s\n%s\n%s\n",ans[0],ans[1],ans[2]);
 
